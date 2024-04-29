4ef3682fc23e881d32a3cec09fb25b6f7a175d83
EC2 Default User <ec2-user@ip-172-31-4-51.ap-northeast-1.compute.internal>
2024-04-29 21:08:55 +0900

    レイアウト作成　ヘッダー、フッター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # def index
+  #   @message = 'Hello, Rails!'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "posts#index"
+ 
+  # get 'posts/index'
+  # devise_for :users
+  # root "posts#index"
+
+  resources :posts do
+    resources :post_comments, only: :create
+  end
 end
